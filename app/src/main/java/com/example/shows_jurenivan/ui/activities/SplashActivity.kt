286ad36f0594edb67459872f718420fc20fbd997
Ivan Juren <ivan.juren@fer.hr>
2019-08-11 11:47:15 +0200

    Handler improvement

--- app/src/main/java/com/example/shows_jurenivan/ui/activities/SplashActivity.kt
+++ app/src/main/java/com/example/shows_jurenivan/ui/activities/SplashActivity.kt
@@ -5,25 +5,36 @@ import android.animation.AnimatorListenerAdapter
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import android.view.animation.*
 import com.example.shows_jurenivan.R
 import kotlinx.android.synthetic.main.activity_splash.*
 
+
 class SplashActivity : AppCompatActivity() {
+
+    companion object {
+        private const val ANIMATION_LEN_FIRST_PART = 1100L
+        private const val ANIMATION_LEN_SECOND_PART = 600L
+    }
+
+    private var handler=Handler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-
-        doBounceAnimation()
     }
 
-    private fun doBounceAnimation() {
+    private fun startAnimation() {
+        handler.postDelayed({
+            startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
+        }, ANIMATION_LEN_FIRST_PART + ANIMATION_LEN_SECOND_PART)
+
         logo.animate()
             .translationY(0f)
             .setInterpolator(BounceInterpolator())
-            .setDuration(1100)
+            .setDuration(ANIMATION_LEN_FIRST_PART)
             .setListener(object : AnimatorListenerAdapter() {
                 override fun onAnimationEnd(animation: Animator?) {
                     doScalingAnimation()
@@ -38,12 +49,18 @@ class SplashActivity : AppCompatActivity() {
             .scaleX(8f)
             .scaleY(8f)
             .setInterpolator(OvershootInterpolator(8f))
-            .setDuration(600)
-            .setListener(object : AnimatorListenerAdapter() {
-                override fun onAnimationEnd(animation: Animator?) {
-                    startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
-                }
-            })
+            .setDuration(ANIMATION_LEN_SECOND_PART)
             .start()
     }
+
+    override fun onPause() {
+        super.onPause()
+        handler.removeCallbacksAndMessages(null)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        startAnimation()
+    }
+
 }
